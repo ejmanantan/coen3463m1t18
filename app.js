5282e4f2d2916031fa3f1f8f860d3504f829f80b
CpE-314 PC24 <CpE-314 PC24>
2016-12-16 14:10:10 +0800

    initial commit

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-    var express = require("express");
+    var express = require('express');
 var path = require ('path');
     var app = express();
 
 app.set('port', (process.env.PORT || 3000));
-app.use(express.static(path.join(__dirname, 'public')))
-app.listen (app.get('port'), function(
+app.use(express.static(path.join(__dirname, 'COEN3463-m1t18')))
+app.listen (app.get('port'), function()
 {
-console.log('Express server is running on port 3000')
-
+console.log('Express server is running on port 3000');
 });
